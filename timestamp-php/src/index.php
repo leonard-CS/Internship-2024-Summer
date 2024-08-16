5dabadb614c6bcdf9e9ba6cea73db0ca63cda1cb
Leonard Chi <lc3223@ic.ac.uk>
2024-08-16 12:46:46 +0800

    Setup initial PHP server with Docker

---
+++ timestamp-php/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Website</h1>
+    <p>This is the homepage.</p>
+    <?php
+    echo "php version: " . phpversion()
+    ?>
+</body>
+</html>
