3f481d749732abd1534d91945e6ce98e8e99b130
Leonard Chi <leonardocompsci@gmail.com>
2024-08-27 16:31:02 +0800

    Create insert_form.php

--- php-mysql/src/insert_data.php
+++ php-mysql/src/insert_data.php
@@ -12,9 +12,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Get data from the form
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+
+// SQL to insert data
 // SQL to insert data
 $sql = "INSERT INTO Users (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john.doe@example.com')";
+VALUES ('$firstname', '$lastname', '$email')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
